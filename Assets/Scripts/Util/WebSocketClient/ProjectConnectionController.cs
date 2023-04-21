d89f8c21c578d29e42c861924ac982c8e0f5b05f
node-value <gumangi3579@gmail.com>
2023-04-22 01:02:41 +0400

    Ckeck if receiver is online

--- Assets/Scripts/Util/WebSocketClient/ProjectConnectionController.cs
+++ Assets/Scripts/Util/WebSocketClient/ProjectConnectionController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using WebSocketSharp;
 using WebSocketSharp.Net;
@@ -10,6 +11,7 @@ public class ProjectConnectionController : MonoBehaviour {
 
     void Start() {
         Connect();
+        SendOnStart();
     }
 
     void Connect() {
@@ -17,13 +19,35 @@ public class ProjectConnectionController : MonoBehaviour {
             ws = new WebSocket(GlobalParams.Map["projectURL"] as string);
             ws.SetCookie(new Cookie("Bearer", GlobalParams.Map["token"] as string));
             ws.OnOpen    += (sender, e) => Debug.Log("Connection opened");
-            ws.OnMessage += (sender, e) => Debug.Log(e.Data);
-            send.onClick.AddListener(SendOnClick);
+            ws.OnMessage += HandleMessage;
+            
             ws.Connect();
         }
     }
 
+    void HandleMessage(object sender, MessageEventArgs e) {
+        ProjectDataDTO response = JsonUtility.FromJson<ProjectDataDTO>(e.Data);
+        if (response.type == ProjectDataMsgType.CHECK) {
+            if (response.data == "OK") {
+                Debug.Log("Requested master is online");
+            } else {
+                Debug.Log("Requested master id offline, either connect to another or load in master mode");
+                SceneManager.LoadScene("Menu");
+                SceneManager.UnloadSceneAsync("MainScene");
+            }
+        }
+    }
+
+    void SendOnStart() {
+        if (GlobalParams.Map.ContainsKey("mode") && (AppMode) GlobalParams.Map["mode"] == AppMode.CONNECT) {
+            ws.Send(JsonUtility.ToJson(new ProjectDataDTO(ProjectDataMsgType.CHECK, GlobalParams.Map["masterUsername"] as string, "")));
+            Debug.Log("Sent to receiver with name: " + GlobalParams.Map["masterUsername"] as string);
+        } else {
+            Debug.Log("SendOnStart failed");
+        }
+    }
+
     void SendOnClick() {
-        ws.Send(JsonUtility.ToJson(new MessageDTO("user1", "hello!")));
+        //ws.Send(JsonUtility.ToJson(new ProjectDataDTO("user1", "hello!")));
     }
 }
---
+++ Assets/Scripts/Util/WebSocketClient/ProjectDataDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Serializable]
+public class ProjectDataDTO {
+
+    public ProjectDataMsgType type;
+    public string receiver, data;
+    
+    public ProjectDataDTO(ProjectDataMsgType type, string receiver, string data) {
+        this.type = type; this.receiver = receiver; this.data = data;
+    }
+}
+
